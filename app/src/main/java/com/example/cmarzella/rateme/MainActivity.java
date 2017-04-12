1edbce2fb747360739e0a6d42ef171acb6e908a1
c.marzella <marzellaclaudio@gmail.com>
2017-04-12 17:44:35 +0200

    aggiunta splash non finita

--- app/src/main/java/com/example/cmarzella/rateme/MainActivity.java
+++ app/src/main/java/com/example/cmarzella/rateme/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.cmarzella.rateme;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView customFontTextView;
+    Typeface typeFace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        typeFace = Typeface.createFromAsset(getAssets(), "font/OpenSans-CondLight.ttf");
+        customFontTextView = (TextView) findViewById(R.id.customFontTextView);
+        customFontTextView.setTypeface(typeFace);
     }
+
 }
